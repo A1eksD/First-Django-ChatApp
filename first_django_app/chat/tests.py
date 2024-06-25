8058859536840d7b0ca9c0a74f9823363716d96b
Aleks <143895611+A1eksD@users.noreply.github.com>
2024-06-25 11:55:42 +0200

    added tests

--- first_django_app/chat/tests.py
+++ first_django_app/chat/tests.py
@@ -1,3 +1,223 @@
-from django.test import TestCase
+from datetime import date
+from django.test import Client, TestCase
+from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+from .models import Message, Chat, newUser
 
-# Create your tests here.
+        
+class ChatTest(TestCase):
+    
+    def test_chatpage(self):
+        self.client = Client()
+        self.user = User.objects.create_user('testuser', password='testuser')
+        self.client.login(username='testuser', password='testuser')
+
+        response = self.client.get('/chat/')
+        self.assertEqual( response.status_code, 200)
+        
+        
+class ChatModelTest(TestCase): 
+    '''
+        Testet, ob ein Chat-Objekt korrekt erstellt wird.
+    '''
+    def test_create_chat(self):
+        chat = Chat.objects.create(create_at=date.today())
+        self.assertEqual(chat.create_at, date.today())
+
+
+class MessageModelTest(TestCase):
+    '''
+        Erstellt zwei Benutzer und ein Chat-Objekt, das für die Tests benötigt wird.
+    '''
+    def setUp(self):
+        self.user1 = get_user_model().objects.create_user(username='user1', password='testpass123')
+        self.user2 = get_user_model().objects.create_user(username='user2', password='testpass123')
+        self.chat = Chat.objects.create(create_at=date.today())
+
+    '''
+        Testet, ob ein Message-Objekt korrekt erstellt wird und die Attribute korrekt gesetzt sind.
+    '''
+    def test_create_message(self):
+        message = Message.objects.create(
+            text="Hello, World!",
+            create_at=date.today(),
+            chat=self.chat,
+            author=self.user1,
+            receiver=self.user2
+        )
+        self.assertEqual(message.text, "Hello, World!")
+        self.assertEqual(message.create_at, date.today())
+        self.assertEqual(message.chat, self.chat)
+        self.assertEqual(message.author, self.user1)
+        self.assertEqual(message.receiver, self.user2)
+
+    '''
+        Testet die Beziehung zwischen Chat und Message und überprüft, ob eine Nachricht in den Nachrichten des entsprechenden Chats enthalten ist.
+    '''
+    def test_chat_relationship(self):
+        message = Message.objects.create(
+            text="Hello, Chat!",
+            create_at=date.today(),
+            chat=self.chat,
+            author=self.user1,
+            receiver=self.user2
+        )
+        self.assertIn(message, self.chat.chat_message_set.all())
+
+
+class newUserModelTest(TestCase):
+
+    '''
+        Testet, ob ein newUser-Objekt korrekt erstellt wird und die Attribute korrekt gesetzt sind.
+    '''
+    def test_create_new_user(self):
+        new_user = newUser.objects.create(
+            name="Test User",
+            email="testuser@example.com",
+            password1="password123",
+            password2="password123"
+        )
+        self.assertEqual(new_user.name, "Test User")
+        self.assertEqual(new_user.email, "testuser@example.com")
+        self.assertEqual(new_user.password1, "password123")
+        self.assertEqual(new_user.password2, "password123")
+        
+     
+        
+class IndexViewTest(TestCase):
+
+    '''
+        Diese Methode richtet die Testumgebung ein, indem sie einen Client, einen Benutzer und einen Chat erstellt.
+    '''
+    def setUp(self):
+        self.client = Client()
+        self.user = get_user_model().objects.create_user(username='testuser', password='testpass123')
+        self.chat = Chat.objects.create(create_at='2023-01-01')
+        self.client.login(username='testuser', password='testpass123')
+        
+    '''
+        Testet, ob ein nicht eingeloggter Benutzer zur Login-Seite weitergeleitet wird, wenn er versucht, die index View aufzurufen.
+    '''    
+    def test_index_view_redirect_if_not_logged_in(self):
+        self.client.logout()
+        response = self.client.get(reverse('index'))
+        self.assertRedirects(response, '/login/?next=/chat/')
+
+    '''
+        Testet, ob ein eingeloggter Benutzer die index View korrekt aufrufen kann und die erwartete HTML-Seite zurückgegeben wird.
+    '''
+    def test_index_view_get_request(self):
+        self.client.login(username='testuser', password='testpass123')
+        response = self.client.get(reverse('index'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'chat/index.html')
+
+    '''
+        Testet, ob eine POST-Anfrage an die index View korrekt verarbeitet wird, eine Nachricht erstellt wird und die erwartete JSON-Antwort zurückgegeben wird.
+    '''
+    def test_index_view_post_request(self):
+        self.client.login(username='testuser', password='testpass123')
+        response = self.client.post(reverse('index'), {'textMessage': 'Hello, World!'})
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'Hello, World!')
+
+        # Check if the message was created
+        message = Message.objects.get(text='Hello, World!')
+        self.assertEqual(message.chat, self.chat)
+        self.assertEqual(message.author, self.user)
+        self.assertEqual(message.receiver, self.user)
+
+    '''
+        Testet, ob eine Nachricht korrekt in der Datenbank erstellt wird, wenn eine POST-Anfrage an die index View gesendet wird.
+    '''
+    def test_index_view_message_creation(self):
+        self.client.login(username='testuser', password='testpass123')
+        response = self.client.post(reverse('index'), {'textMessage': 'Test message'})
+        self.assertEqual(response.status_code, 200)
+
+        message = Message.objects.get(text='Test message')
+        self.assertEqual(message.text, 'Test message')
+        self.assertEqual(message.chat, self.chat)
+        self.assertEqual(message.author, self.user)
+        self.assertEqual(message.receiver, self.user)
+        
+        
+class LoginViewTests(TestCase):
+    
+    def setUp(self):
+        self.client = Client()
+        self.login_url = reverse('login_view')
+        self.chat_url = reverse('index')
+        self.user = User.objects.create_user(username='testuser', password='testpassword')
+
+    def test_login_valid_user(self):
+        # Teste den Login mit gültigen Daten
+        response = self.client.post(self.login_url, {'username': 'testuser', 'password': 'testpassword', 'redirect': '/chat/'})
+        self.assertRedirects(response, self.chat_url)
+
+    def test_login_invalid_user(self):
+        # Teste den Login mit ungültigen Daten
+        response = self.client.post(self.login_url, {'username': 'invaliduser', 'password': 'wrongpassword', 'redirect': '/chat/'})
+        self.assertTemplateUsed(response, 'auth/login.html')
+        self.assertTrue(response.context['wrongData'])
+
+    def test_login_redirect(self):
+        # Teste, ob nach dem Login eine Weiterleitung erfolgt
+        response = self.client.post(self.login_url, {'username': 'testuser', 'password': 'testpassword', 'redirect': '/chat/'})
+        self.assertRedirects(response, self.chat_url)
+
+    def test_login_view_get(self):
+        # Teste den GET-Request auf die Login-Seite
+        response = self.client.get(self.login_url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'auth/login.html')
+        
+        
+class RegisterViewTests(TestCase):
+
+    def setUp(self):
+        self.client = Client()
+        self.register_url = reverse('register_view')
+        self.chat_url = reverse('index')
+
+    def test_register_valid_user(self):
+        # Teste die Registrierung mit gültigen Daten
+        response = self.client.post(self.register_url, {
+            'registerName': 'testuser',
+            'registerEmail': 'test@example.com',
+            'registerPassword1': 'testpassword',
+            'registerPassword2': 'testpassword',
+            'next': '/chat/'
+        })
+        self.assertRedirects(response, self.chat_url)
+
+        # Überprüfe, ob der Benutzer tatsächlich erstellt wurde
+        self.assertTrue(User.objects.filter(username='testuser').exists())
+
+    def test_register_password_mismatch(self):
+        # Teste die Registrierung mit Passwörtern, die nicht übereinstimmen
+        response = self.client.post(self.register_url, {
+            'registerName': 'testuser',
+            'registerEmail': 'test@example.com',
+            'registerPassword1': 'testpassword1',
+            'registerPassword2': 'testpassword2',
+            'next': '/chat/'
+        })
+        self.assertTemplateUsed(response, 'auth/register.html')
+        self.assertIn('error', response.context)
+        self.assertEqual(response.context['error'], 'Passwords do not match.')
+
+        # Überprüfe, ob der Benutzer nicht erstellt wurde
+        self.assertFalse(User.objects.filter(username='testuser').exists())
+
+    def test_register_exception_handling(self):
+        # Teste die Behandlung von Ausnahmen während der Registrierung
+        # Hier könnte man z.B. einen Test mit ungültigen Daten oder anderen Fehlern ergänzen
+        pass  # Hier müsste entsprechender Testcode eingefügt werden
+
+    def test_register_view_get(self):
+        # Teste den GET-Request auf die Registrierungsseite
+        response = self.client.get(self.register_url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'auth/register.html')
--- first_django_app/chat/views.py
+++ first_django_app/chat/views.py
@@ -1,68 +1,62 @@
-from mailbox import Message
 from django.http import HttpResponseRedirect, JsonResponse
 from django.shortcuts import render
-from .models import Message, Chat
+from .models import Message as ChatMessage, Chat
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from django.core import serializers
 
-
-
-
-
-@login_required(login_url='/login/') # man leitet automatisch zu /login/ weiter, wenn man nicht eingeloggt ist
-
-# Create your views here.
+@login_required(login_url='/login/')  # man leitet automatisch zu /login/ weiter, wenn man nicht eingeloggt ist
 def index(request):
     if request.method == 'POST':
-        print("Received date " + request.POST['textMessage'])
-        # vergebe immer den chat mit id=1 an die message
+        print("Received data " + request.POST['textMessage'])
+        # vergebe immer den Chat mit id=1 an die Message
         myChats = Chat.objects.get(id=1)
-        # erstelle gleichzitig ein neues textObject
-        new_message = Message.objects.create(text=request.POST['textMessage'], chat=myChats, author=request.user, receiver=request.user  )
-        serialized_obj = serializers.serialize('json', [ new_message, ]) # wandel die message in ein array um
-        return JsonResponse(serialized_obj[1:-1], safe=False) # gib das json als wtring wieder / [1:-1] entfernt die [] und returnt einen reinen json und kein string
-    chatMessages = Message.objects.filter(chat__id=1) #chat__id=1  ==  syntax um auf chat mit der id 1 zuzugreifen
-
+        # erstelle gleichzeitig ein neues Text-Objekt
+        new_message = ChatMessage.objects.create(
+            text=request.POST['textMessage'],
+            chat=myChats,
+            author=request.user,
+            receiver=request.user
+        )
+        serialized_obj = serializers.serialize('json', [new_message])
+        return JsonResponse(serialized_obj[1:-1], safe=False)
+    
+    chatMessages = ChatMessage.objects.filter(chat__id=1)
     return render(request, 'chat/index.html', {'messages': chatMessages})
 
-
 def login_view(request):
-    redirect = request.GET.get('next', '/chat/') #weiterleitung (next im link nicht vorhaned, deshalb chat als ausweichmöglichkeit)
+    redirect = request.GET.get('next', '/chat/')
     if request.method == 'POST':
         user = authenticate(username=request.POST.get('username'), password=request.POST.get('password'))
-        print('user ' , user) # check user angaben
+        print('user', user)  # check user Angaben
         if user:
-            login(request, user) # überprüfe user in der datenbank
-            return HttpResponseRedirect(request.POST.get('redirect', '/chat/')) # weiterleitung
+            login(request, user)  # überprüfe user in der Datenbank
+            return HttpResponseRedirect(request.POST.get('redirect', '/chat/'))  # Weiterleitung
         else:
-            return render(request, 'auth/login.html', {'wrongData': True, 'redirect': redirect}) # return fehler, wenn was nicht passt
+            return render(request, 'auth/login.html', {'wrongData': True, 'redirect': redirect})
     return render(request, 'auth/login.html', {'redirect': redirect})
-
 
 def register_view(request):
     redirect = request.GET.get('next', '/chat/')
     if request.method == 'POST':
-        # lese die daten aus
+        # lese die Daten aus
         username = request.POST.get('registerName')
         email = request.POST.get('registerEmail')
         password1 = request.POST.get('registerPassword1')
         password2 = request.POST.get('registerPassword2')
 
-        # überprüfe ob pw's gleich sind
-        if password1 != password2: 
+        # überprüfe ob PW's gleich sind
+        if password1 != password2:
             return render(request, 'auth/register.html', {'error': 'Passwords do not match.', 'redirect': redirect})
 
         try:
-            user = User.objects.create_user(username, email, password1) #lege den user mit create_user an
-            login(request, user) # überprüfe user in der datenbank
-            return HttpResponseRedirect(redirect) # weiterleitung
+            user = User.objects.create_user(username=username, email=email, password=password1)  # lege den User an
+            login(request, user)  # logge den User ein
+            return HttpResponseRedirect(redirect)  # Weiterleitung
         except Exception as e:
             # Handle other registration errors
             print(f"Registration error: {e}")
-            return render(request, 'auth/register.html', {'error': str(e)})  # zeige error
+            return render(request, 'auth/register.html', {'error': str(e), 'redirect': redirect})  # zeige Fehler
 
     return render(request, 'auth/register.html', {'redirect': redirect})
-
-
